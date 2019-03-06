ceafb198beef72705897a5a9a9cc9f3b900de896
lizonghang <870644199@qq.com>
2019-03-06 18:40:04 +0800

    add hybrid sequential

---
+++ efficiency/run_hybrid_sequential.py
@@ -0,0 +1,29 @@
+from mxnet import nd, sym
+from mxnet.gluon import nn
+import time
+
+
+def get_net():
+    net = nn.HybridSequential()
+    net.add(nn.Dense(256, activation="relu"),
+            nn.Dense(128, activation="relu"),
+            nn.Dense(2))
+    return net
+
+
+def benchmark(net, X):
+    start = time.time()
+    for i in xrange(1000):
+        _ = net(X)
+    nd.waitall()
+    return time.time() - start
+
+
+if __name__ == "__main__":
+    X = nd.random.normal(scale=0.01, shape=(1, 512))
+    net = get_net()
+    net.initialize()
+    print 'before hybridizing: %.4f' % benchmark(net, X)
+    net.hybridize()
+    print 'after hybridizing: %.4f' % benchmark(net, X)
+    net.export('sym-hybrid-sequential')
