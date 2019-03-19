a2512aab95617468b45eee0c8f691f8046f5dfe4
lizonghang <870644199@qq.com>
2019-03-19 16:28:42 +0800

    add kvstore local

---
+++ distributed/run_kvstore_local.py
@@ -0,0 +1,28 @@
+from mxnet import kv, nd, gpu
+
+if __name__ == "__main__":
+    # MXNet provides a key-value store to synchronize data among devices.
+    # The following code initializes an ndarray associated with the key “weight” on a key-value store.
+    print('running kvstore local')
+    kv_local = kv.create("local")
+    SHAPE = (2, 3)
+    x = nd.random.uniform(shape=SHAPE)
+    kv_local.init("params", x)
+    print('=== init "params" ==={}'.format(x))
+    # After initialization, we can pull the value to multiple devices.
+    NUM_GPUS = 2
+    ctx = [gpu(i) for i in range(NUM_GPUS)]
+    y = [nd.zeros(shape=SHAPE, ctx=c) for c in ctx]
+    kv_local.pull("params", out=y)
+    print('=== pull "params" to {} ===\n{}'.format(ctx, y))
+    # We can also push new data value into the store.
+    # It will first sum the data on the same key and then overwrite the current value.
+    z = [nd.ones(shape=SHAPE, ctx=c) for c in ctx]
+    kv_local.push("params", z)
+    print('=== push to "params" ===\n{}'.format(z))
+    kv_local.pull("params", out=y)
+    print('=== pull "params" ===\n{}'.format(y))
+    # With push and pull we can define the allreduce function by
+    # def allreduce(data, data_name, store):
+    #     store.push(data_name, data)
+    #     store.pull(data_name, out=data)
